a29f2dfa077ed193c9d63b113bbe6499c81ebaf4
Rebecca Eakins <rebecca.a.eakins@gmail.com>
2015-12-27 19:56:58 -0600

    completes voidmethods BookBanner tests

--- src/main/java/com/thoughtworks/tddintro/voidmethods/BookBanner.java
+++ src/main/java/com/thoughtworks/tddintro/voidmethods/BookBanner.java
@@ -3,11 +3,16 @@ package com.thoughtworks.tddintro.voidmethods;
 import java.util.Collection;
 
 public class BookBanner {
-    public BookBanner(Library library) {
+    private Library library;
 
+    public BookBanner(Library library) {
+        this.library = library;
     }
 
     public void remove(Collection<String> bannedBooks) {
+        for(String book : bannedBooks){
+            library.removeBook(book);
+        }
 
     }
 }
